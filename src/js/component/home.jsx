f5b6255e185befcee51305de06dac439510749bf
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-08-27 19:14:39 +0000

    cambios finales

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,42 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
+// Incluye imágenes en tu bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState ("")
-	const [todos, setTodos] = useState ([])
-	return (
-	<div className="container">
-		<h1>My to do list</h1>
-		<ul>
-			<li>
-			<input 
-				type="text"
-				onChange={(e)=>setInputValue(e.target.value)}
-				value= {inputValue}
-				onKeyPress={(e)=> {
-					if (e.key === "Enter") {
-						setTodos(todos.concat([inputValue]));
-						setInputValue("");
-					}
-				}}
-				placeholder="What do you need to do?"></input>
-				</li>
-			{todos.map((item) => 
-				(
-					<li>
-						{item} <i class="fas fa-trash-alt" onClick={() => setTodos((todos.filter(t, currentIndex) => index != currentIndex)
-						)}></i>
-					</li>
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
 
-			))}
-			
-		</ul>
-			<div>{todos.length} tasks</div>
-	</div>)
-
-};
+    return (
+        <div className="container">
+            <h1>My to do list</h1>
+            <ul>
+                <li>
+                    <input 
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                setTodos(todos.concat([inputValue]));
+                                setInputValue("");
+                            }
+                        }}
+                        placeholder="What do you need to do?"
+                    />
+                </li>
+                {todos.map((item, index) => (
+                    <li key={index}>
+                        {item} 
+                        <i 
+                            className="fas fa-trash-alt"
+                            onClick={() => setTodos(
+                                todos.filter((_, currentIndex) => index !== currentIndex)
+                            )}
+                        ></i>
+                    </li>
+                ))}
+            </ul>
+            <div>{todos.length} tasks left</div>
+        </div>
+    );
+}
 
 export default Home;
